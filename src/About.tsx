17ea74cdaf974f7afdfe75c8db07970a952a76bd
rayray39 <raynerlimfy@gmail.com>
2025-03-04 19:12:48 +0800

    About.tsx: Make fontsizes local constants

--- src/About.tsx
+++ src/About.tsx
@@ -1,18 +1,21 @@
 import { Box, Stack } from "@mui/material"
 
 function About() {
+    const SMALL_SECTION_FONTSIZE = '30px';
+    const SMALL_SECTION_CONTENT_FONTSIZE = '20px';
+
     return <>
         <Stack>
             <Box sx={{
                 fontWeight:'bold',
-                fontSize:'24px',
+                fontSize: SMALL_SECTION_FONTSIZE,
                 marginTop:'40px'
             }}>
                 About Me
             </Box>
 
             <Box sx={{
-                fontSize:'20px'
+                fontSize: SMALL_SECTION_CONTENT_FONTSIZE
             }}>
                 I'm passionate about building innovative fullstack applications and robotics development.
                 I've a solid background in engineering, computer science and robotics.
