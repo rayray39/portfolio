2467adc9f015928dd7c4697882976a4b6f6fd460
rayray39 <raynerlimfy@gmail.com>
2025-03-05 10:01:41 +0800

    App.tsx: Make page scrollable in y direction

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,21 @@ import { Stack } from "@mui/material"
 import Header from "./Header"
 import About from "./About"
 import Techstack from "./Techstack"
+import Projects from "./Projects"
 
 function App() {
     return <Stack sx={{ 
         width: '50%',
         display:'flex',
         flexDirection:'column',
-        alignItems:'center',
+        alignItems: 'start',
         justifyContent:'center',
-        position:'fixed',
         transform: 'translate(50%, 0)',
     }}>
         <Header />
         <About />
         <Techstack />
+        <Projects />
     </Stack>
 }
 
