6caa325ab11b5572b32b7a05cb47c8d119668cf2
rayray39 <raynerlimfy@gmail.com>
2025-03-05 18:58:07 +0800

    Projects.tsx: Add custom card component to display project

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -4,6 +4,15 @@ function Projects() {
     const SMALL_SECTION_FONTSIZE = '30px';
     const SMALL_SECTION_CONTENT_FONTSIZE = '20px';
 
+    const pengChipLabels = [
+        <CustomChip label="ReactJS" />,
+        <CustomChip label="Express.JS" />,
+        <CustomChip label="SQLite3" />,
+        <CustomChip label="RESTful APIs" />,
+        <CustomChip label="Material UI" />,
+        <CustomChip label="Vercel" />
+    ]
+
     return <>
         <Stack sx={{
             marginTop:'40px'
@@ -33,33 +42,42 @@ function Projects() {
                 </Box>
             </Stack>
             
-            <Card sx={{ width:'100%' }}>
-                <CardMedia
-                    sx={{ height: 220 }}
-                    image="/src/assets/peng_logo.jpg"
-                    title="Peng"
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        Peng
-                    </Typography>
-                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                        Peng is a no nonsense dating platform designed for those looking to make a serious connection.
-                        Features include authentication via JWT (Json Web Tokens), real-time messaging, interactive
-                        and dynamic swiping features.
-                    </Typography>
-                    <Stack direction={'row'} spacing={2} sx={{ marginTop:'10px'}}>
-                        <Chip label='ReactJS'/>
-                        <Chip label='Express.JS'/>
-                        <Chip label='SQLite3'/>
-                        <Chip label='RESTful APIs'/>
-                        <Chip label='Material UI'/>
-                        <Chip label='Vercel'/>
-                    </Stack>
-                </CardContent>
-            </Card>
+            <CustomCard 
+                imagePath="peng_logo.jpg" 
+                title="Peng" 
+                description="Peng is a no nonsense dating platform designed for those looking to make a serious connection.
+                Features include authentication via JWT (Json Web Tokens), real-time messaging, interactive
+                and dynamic swiping features."
+                customChipLabels={pengChipLabels}
+            />
         </Stack>
     </>
 }
 
 export default Projects
+
+function CustomCard({ imagePath, title, description, customChipLabels }:
+        { imagePath:string, title:string, description:string, customChipLabels:React.ReactNode[] }) {
+    return <Card sx={{ width:'100%' }}>
+        <CardMedia
+            sx={{ height: 220 }}
+            image={imagePath}
+            title={title}
+        />
+        <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+                {title}
+            </Typography>
+            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                {description}
+            </Typography>
+            <Stack direction={'row'} spacing={2} sx={{ marginTop:'10px' }}>
+                {customChipLabels}
+            </Stack>
+        </CardContent>
+    </Card>
+}
+
+function CustomChip({ label }: { label:string }) {
+    return <Chip label={label} sx={{bgcolor:'black', color:'white'}} />
+}
