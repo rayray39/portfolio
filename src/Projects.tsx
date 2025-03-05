19aa6c398e9d864ecd98a3d6707c38e37d435bde
rayray39 <raynerlimfy@gmail.com>
2025-03-05 10:26:47 +0800

    Projects.tsx: Add MUI Card for project

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,4 +1,4 @@
-import { Box, Chip, Stack } from "@mui/material"
+import { Box, Card, CardContent, CardMedia, Chip, Stack, Typography } from "@mui/material"
 
 function Projects() {
     const SMALL_SECTION_FONTSIZE = '30px';
@@ -11,7 +11,8 @@ function Projects() {
             <Stack sx={{
                 display:'flex',
                 flexDirection:'column',
-                alignItems:'center'
+                alignItems:'center',
+                marginBottom:'40px'
             }}>
                 <Chip label="My Projects" sx={{width:'16%', bgcolor:'black', color:'white'}}/>
                 <Box sx={{
@@ -22,15 +23,34 @@ function Projects() {
                 }}>
                     Featured Projects
                 </Box>
+                <Box sx={{
+                    fontSize:SMALL_SECTION_CONTENT_FONTSIZE,
+                    textAlign:'center',
+                    marginTop: '10px',
+                    width:'70%'
+                }}>
+                    I build projects from 0 to 1, turning ideas into reality. Take a look at some of my favourite projects below.
+                </Box>
             </Stack>
             
-            
+            <Card sx={{ width:'100%' }}>
+                <CardMedia
+                    sx={{ height: 220 }}
+                    image="/src/assets/peng_logo.jpg"
+                    title="Peng"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Peng
+                    </Typography>
+                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+            </Card>
         </Stack>
     </>
 }
 
 export default Projects
-
-function Card() {
-
-}
