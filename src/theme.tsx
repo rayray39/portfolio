c0d090c7750eea04f71b000230a41ed74e96ad0c
rayray39 <raynerlimfy@gmail.com>
2025-05-24 15:29:15 +0800

    main.tsx: Add custom theme for global font family

---
+++ src/theme.tsx
@@ -0,0 +1,12 @@
+
+// theme.ts
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Ubuntu mono, monospace',
+  },
+  // You can also customize other typography settings here if needed
+});
+
+export default theme;
